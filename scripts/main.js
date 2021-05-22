76155ccc2842e98892f3fe2c33c80a13f3c95cce
Dima Alhimov <alhimovd877@yandex.ru>
2021-05-22 16:05:35 +0300

    Исправить замечания

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,34 @@ let profTitle = document.querySelector('.profile__title');
 let profSubtitle = document.querySelector('.profile__subtitle');
 let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close-icon');
-let nameInput = document.querySelector('.form__user');
-let jobInput = document.querySelector('.form__job');
-let formButton = document.querySelector('.popup__button');
-// Слушатель для открытия попапа с данными в форме 
-openPopup.addEventListener('click', function(){
+let nameInput = document.querySelector('.form__item_el_name');
+let jobInput = document.querySelector('.form__item_el_job');
+let formElement = document.querySelector('.form')
+
+// Функция для открытия попапа 
+function openedPopup () {
   popup.classList.add('popup_opened');
   nameInput.value = profTitle.textContent;
   jobInput.value = profSubtitle.textContent;
-})
-// Закрытие попапа
-closePopup.addEventListener('click', function(){
+}
+// Функция для закрытия попапа
+function closePopupIcon (){
   popup.classList.remove('popup_opened');
-})
+}
+// Слушатель для открытия попапа с данными в форме 
+openPopup.addEventListener('click', openedPopup);
+  
+// Слушатель для закрытия попапа с данными в форме Закрытие попапа
+closePopup.addEventListener('click', closePopupIcon);
+  
+
 // Функция для изменения донных в профиле из формы и зякрытие попапа evt.preventDefault() отменяет стандартную отправку формы 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profTitle.textContent = nameInput.value;
   profSubtitle.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closePopupIcon ();
 }
-// нажимаем на кнопку сохранить в форме.
-formButton.addEventListener('click', formSubmitHandler);
+// Отправа формы
+formElement.addEventListener('submit', formSubmitHandler);
+ 
