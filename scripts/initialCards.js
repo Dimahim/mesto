b9dab96e82dc75c6d443b1e899c2f9fa9634dca4
Dima Alhimov <alhimovd877@yandex.ru>
2021-07-05 17:14:40 +0300

    Перенести в отдельный файл массив

---
+++ scripts/initialCards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    link: './images/kirill-pershin-1088404.jpg',
+    name: 'Карачаево-Черкесия'
+  },
+  {
+    link: './images/kirill-pershin-1404681-unsplash.png',
+    name: 'Гора Эльбрус'
+  },
+  {
+    link: './images/kirill-pershin-1556355-unsplash.jpg',
+    name: 'Домбай'
+  },
+  {
+    link: './images/photo-grid-baikal-2.jpg',
+    name: 'Байкал'
+  },
+  {
+    link: './images/photo-grid-sochi.jpg',
+    name: 'Сочи'
+  },
+  {
+    link: './images/place-karelia.jpg',
+    name: 'Карелия'
+  },
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,4 @@
-// cоздаем массив__________________________________________
-const initialCards = [
-  {
-    link: './images/kirill-pershin-1088404.jpg',
-    name: 'Карачаево-Черкесия'
-  },
-  {
-    link: './images/kirill-pershin-1404681-unsplash.png',
-    name: 'Гора Эльбрус'
-  },
-  {
-    link: './images/kirill-pershin-1556355-unsplash.jpg',
-    name: 'Домбай'
-  },
-  {
-    link: './images/photo-grid-baikal-2.jpg',
-    name: 'Байкал'
-  },
-  {
-    link: './images/photo-grid-sochi.jpg',
-    name: 'Сочи'
-  },
-  {
-    link: './images/place-karelia.jpg',
-    name: 'Карелия'
-  },
-];
+
 const profilePopupOpenButton = document.querySelector('.profile__edit-button');
 const profTitle = document.querySelector('.profile__title');
 const profSubtitle = document.querySelector('.profile__subtitle');
